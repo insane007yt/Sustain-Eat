6ea5d778b41e9267a6df9070ab0bd1b7bf64e93f
Food Analyzer Team <developer@foodanalyzer.local>
2026-02-25 02:30:58 +0530

    Restore original Main.java from decompiled class (full implementation)

--- src/com/foodanalyzer/app/Main.java
+++ src/com/foodanalyzer/app/Main.java
@@ -1,7 +1,44 @@
 package com.foodanalyzer.app;
 
+import com.foodanalyzer.ui.MainFrame;
+import java.awt.Component;
+import java.awt.Insets;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Food Analyzer - Java source placeholder");
+        setupLookAndFeel();
+        SwingUtilities.invokeLater(() -> {
+            try {
+                MainFrame frame = new MainFrame();
+                frame.setVisible(true);
+            } catch (Exception var1) {
+                var1.printStackTrace();
+                JOptionPane.showMessageDialog((Component)null, "Failed to start application: " + var1.getMessage(), "Error", 0);
+            }
+
+        });
+    }
+
+    private static void setupLookAndFeel() {
+        try {
+            System.setProperty("awt.useSystemAAFontSettings", "on");
+            System.setProperty("swing.aatext", "true");
+            System.setProperty("sun.java2d.opengl", "true");
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            UIManager.put("Button.arc", 12);
+            UIManager.put("Component.arc", 8);
+            UIManager.put("TextComponent.arc", 8);
+            UIManager.put("ScrollBar.thumbArc", 999);
+            UIManager.put("ScrollBar.thumbInsets", new Insets(2, 2, 2, 2));
+        } catch (Exception var1) {
+            System.err.println("Could not set look and feel: " + var1.getMessage());
+        }
+
     }
 }
